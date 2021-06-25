a75fe20f13cf69f1d449a2a19b19e31774fdd036
ceceba223 <hagen-gerlatzek@web.de>
2021-06-26 00:31:44 +0200

    removed hard-coded var

--- save_load.py
+++ save_load.py
@@ -3,15 +3,17 @@ from Crypto.PublicKey import RSA
 from encryption import encrypt_data
 from encryption import decrypt_data
 
+path = './secret/passwords/'
+
 def save(passwd, name):													#save pw to ./secret/passwords/
 	passwd = encrypt_data(passwd)										#encrypt passwd
-	with open('./secret/passwords/' + name + '.txt', 'wb') as file:		#create file for password
+	with open(path + name + '.txt', 'wb') as file:		#create file for password
 		file.write(passwd)												#save password to file
 		file.close()													#close file
 	print(f"\nYour password has been saved as \033[36m{name}\033[0m\n\n")
 
 def load(name):															#load pw from file
-	try: file = open('./secret/passwords/' + name + '.txt', 'rb')		#try to open file
+	try: file = open(path + name + '.txt', 'rb')		#try to open file
 	except FileNotFoundError:											#in case password file does not exist
 		print("This password does not exist\n\n")
 		return()
